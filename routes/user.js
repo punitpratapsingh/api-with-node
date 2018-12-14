898c22f93f39db191724a08df16c06669db78ab6
punit <punitpratapsingh30@gmail.com>
2018-12-14 15:03:57 +0530

    commit

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,9 @@ const userController = require('../controllers/user');
 //dependiencies
 module.exports = (app) => {
     app.post('/create', userController.create);
-    app.post('/delete', userController.delete);
-    app.post('/details', userController.details);
-    app.post('/update', userController.update);
+    app.delete('/delete', userController.delete);
+    app.get('/details', userController.details);
+    app.put('/update', userController.update);
 }
 
 
